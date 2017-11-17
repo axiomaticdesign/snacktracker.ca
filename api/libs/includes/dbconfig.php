ad6e59fd8440a1fa0e13af782ba2aadc97a01c9b
Cem Bakar <cem@tulip.io>
2017-11-17 13:19:12 -0500

    DB structure is added

---
+++ api/libs/includes/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+	define( 'DB_TYPE', 'mysql' );
+	define( 'DB_HOST', 'mysql.snacktracker.ca' );
+	define( 'DB_NAME', 'snacktracker' );
+	define( 'DB_USER', 'tulipst' );
+	define( 'DB_PASS', 'pradaposse' );
+	define( 'CHARSET', 'utf8' );
+	
---
+++ api/libs/includes/utils.php
@@ -0,0 +1,3 @@
+<?php
+	define("TWILIO_API_KEY", "AZXDES");
+	define("TWILIO_SECRET_KEY", "asdfasdfasdf");
